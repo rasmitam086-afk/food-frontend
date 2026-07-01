5e8724e09e07c4b92a9a124d215743cb78997cd6
Rasmita maharana <rasmitam086@gmail.com>
2026-07-01 13:21:42 +0530

    test-2

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import AdminPanel from "./pages/AdminPanel";
 import AdminProtectedRoute from "./pages/AdminProtectedRoute"
 import Dashboard from "./Components/Dashboard";
 import Food from "./Components/Food";
-import AddFoodPage from "./Components/AddFoodPage";
+import AddFoodPage from "../src/Components/AddFoodPage.jsx"
 import EditFoodPage from "./Components/EditFoodPage";
 import DetailsPage from "./Layout/DetailsPage";
 import Order from "./pages/Order";
